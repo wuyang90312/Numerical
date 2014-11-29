c4be55183f0727ac8b4e80ba93e9e49abaa78cb8
Yang <wuyangcanada@hotmail.com>
2014-11-28 21:26:31 -0500

    Hermite Polynomial Done
    Print out 5 successive polynomials each of which corresponds to a
    sub-domain

--- Hermite.c
+++ Hermite.c
@@ -1,85 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#define		SUCCESS		0
-#define		FAILURE		-1
+#define		SUCCESS					0
+#define		FAILURE					-1
+#define		POLYNOMIAL_DEPTH		3
 
-static const double X_COORD[6] = { 0, 0.2, 0.4, 0.6, 0.8, 1.0 };
-static const double Y_COORD[6] = { 0, 14.7, 36.5, 71.7, 121.4, 197.4 };
+static const double X_COORD[6] = { 0, 1.3, 1.4, 1.7, 1.8, 1.9 };
+static const double Y_COORD[6] = { 0, 540.6, 1062.8, 8687.4, 13924.3, 22650.2 };
 
-//static const double X_COORD[6] = { 0, 1.3, 1.4, 1.7, 1.8, 1.9 };
-//static const double Y_COORD[6] = { 0, 540.6, 1062.8, 8687.4, 13924.3, 22650.2 };
-
-double CoefficientCalculation(int index);
-void fillItem(int index, double* target);
+void differentaition(double* derivative);
+void fillItem(double a, double b, double c, double* target);
 double	combinationCalculation(int depth, int location, double* input);
+void polynomialGeneration(double* item, double* constant, double coefficient);
+void cleanUp(int length, double* target);
 
 int main()
 {
-	int empty,loop, loop1, depth;
-	double sum, tmp, coefficient, *constant;
-	double item[6] = {0,0,0,0,0,0};
+	int		loop, iteration = 0;
+	double *derivative, a1, b1, a2, b2,x1,x2, coefficient, *equation;
+	double item[6] = { 0, 0, 0, 0, 0, 0 }, constant[3] = {0, 0, 0};
 
-	constant = malloc(5 * sizeof(double));
-
-	for (empty = 0; empty < 6; empty++)
+	derivative = malloc(6*sizeof(double)); /* Declare the memory location for the derviative value of each point*/
+	equation = calloc(4, sizeof(double)); /* Declare the memory location for the polynomial */
+	differentaition(derivative); /* Put all derivatives into the array */
+	
+	for (iteration = 0; iteration < 5; iteration++)
 	{
-		coefficient = CoefficientCalculation(empty);
-		fillItem(empty, constant); /* Withdraw a cell out of the array */
+		/* Obtain the coefficient from Y and derivative */
+		a1 = Y_COORD[iteration];
+		a2 = Y_COORD[iteration + 1];
+		b1 = derivative[iteration];
+		b2 = derivative[iteration + 1];
+		x1 = X_COORD[iteration];
+		x2 = X_COORD[iteration + 1];
 
-		/* Add coefficient of each order to the array */
-		for (loop = 0; loop < 6; loop++)
-		{
-			sum = 0;
+		/* Get the value of the constant */
+		coefficient = 1 / pow(x1 - x2, 2);
 
-			if (loop == 0)
-				depth = 0;
-			else
-				depth = 5 - loop;
+		/* Get the coefficient of V_1 */
+		fillItem(x1, x2, x2, constant);
+		polynomialGeneration(equation, constant, b1*coefficient);
+		
+		/* Get the coefficient of V_2 */
+		fillItem(x1, x1, x2, constant);
+		polynomialGeneration(equation, constant, b2*coefficient);
+		
+		/* Get the coefficient of U_1 */
+		fillItem((3 * x1 - x2) / 2, x2, x2, constant);
+		coefficient = -2.0 / pow(x1 - x2, 3);
+		polynomialGeneration(equation, constant, a1*coefficient);
+		
+		/* Get the coefficient of U_2 */
+		fillItem((3 * x2 - x1) / 2, x1, x1, constant);
+		coefficient = -2.0 / pow(x2 - x1, 3);
+		polynomialGeneration(equation, constant, a2*coefficient);
 
-			for (loop1 = 0; loop1 <= depth; loop1++)
-			{
-				sum += combinationCalculation(loop, loop1, constant);
-			}
-			item[loop] += sum*coefficient;
-		//	printf("\n");
-		}
+		printf("Polynomial is:\n %fX^3 + %fX^2 + %fX +%f\n", equation[0], equation[1], equation[2], equation[3]);
+		printf("\n");
+		cleanUp(3, equation);
 	}
 
-	printf("Polynomial is:\n%fX^5 + %fX^4 + %fX^3 + %fX^2 + %fX +%f", item[0], item[1], item[2], item[3], item[4], item[5]);
 	return SUCCESS;
 }
 
-/* calculate the result of  Y_j/((X_j - X_1)(X_j - X_2)...(X_j - X_n)) */
-double CoefficientCalculation(int index)
+/* Give the derivative values of 6 points */
+void differentaition(double* target)
 {
-	int loop = 5;
-	double result = 1;
-
-	for (; loop >= 0; loop--)
-	{
-		if (loop == index)
-			result *= Y_COORD[loop];
-		else
-			result /= (X_COORD[index] - X_COORD[loop]);
-	}
-	return result;
-}
-
-void fillItem(int index, double* target)
-{
-	int loop, count = 0;
+	int loop;
 
 	for (loop = 0; loop < 6; loop++)
 	{
-		if (loop == index)
-			continue;
-		else
-		{
-			target[count] = -X_COORD[loop];
-			count++;
-		}
+		if (loop == 0) /* If the point locates at either end, calculate the derivative based on a adjacent point and itself */
+			target[loop] = (Y_COORD[1]-Y_COORD[0]) / (X_COORD[1] - X_COORD[0]);
+		else if (loop == 5)
+			target[loop] = (Y_COORD[5] - Y_COORD[4]) / (X_COORD[5] - X_COORD[4]);
+		else /* otherwise, calculate the derivative based on both adjacent points */
+			target[loop] = (Y_COORD[loop + 1] - Y_COORD[loop - 1]) / (X_COORD[loop + 1] - X_COORD[loop - 1]);
 	}
+}
+
+/* Clean up all of the cells */
+void cleanUp(int length, double* target)
+{
+	for (; length >= 0; length--)
+		target[length] = 0;
+}
+
+void fillItem(double a, double b, double c, double* target)
+{
+	target[0] = -a;
+	target[1] = -b;
+	target[2] = -c;
+
 }
 double	combinationCalculation(int depth, int location, double* input)
 {
@@ -102,9 +115,9 @@ double	combinationCalculation(int depth, int location, double* input)
 	}		
 	else
 	{
-		for (loop = location + 1; loop < 5; loop++)
+		for (loop = location + 1; loop < POLYNOMIAL_DEPTH; loop++)
 		{
-			if (depth + loop <= 5)
+			if (depth + loop <= POLYNOMIAL_DEPTH)
 			{
 				sum += combinationCalculation(depth, loop, input);
 			}
@@ -115,4 +128,26 @@ double	combinationCalculation(int depth, int location, double* input)
 	result *= sum;
 
 	return result;
+}
+
+void polynomialGeneration(double* item, double* constant, double coefficient)
+{
+	int loop, sum, depth, loop1;
+
+	/* Add coefficient of each order to the array */
+	for (loop = 0; loop <= POLYNOMIAL_DEPTH; loop++)
+	{
+		sum = 0;
+
+		if (loop == 0)
+			depth = 0;
+		else
+			depth = POLYNOMIAL_DEPTH - loop;
+
+		for (loop1 = 0; loop1 <= depth; loop1++)
+		{
+			sum += combinationCalculation(loop, loop1, constant);
+		}
+		item[loop] += sum*coefficient;
+	}
 }
